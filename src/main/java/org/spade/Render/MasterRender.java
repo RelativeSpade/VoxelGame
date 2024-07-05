1dbadfce820044a2364cb37ee8bb5a39caf25724
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 04:45:11 -0400

    MasterRender

--- src/main/java/org/spade/Render/MasterRender.java
+++ src/main/java/org/spade/Render/MasterRender.java
@@ -1,4 +1,13 @@
 package org.spade.Render;
 
+import org.lwjgl.opengl.GL11;
+
 public class MasterRender {
+
+    public void prepare() {
+
+        GL11.glClearColor(0.4f, 0.7f, 1.0f, 1);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+    }
 }
