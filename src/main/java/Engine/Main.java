2991b340752ad0908760b8fc559cac0627aa3929
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 05:36:10 -0400

    empty window

--- src/main/java/Engine/Main.java
+++ src/main/java/Engine/Main.java
@@ -1,12 +1,26 @@
 package Engine;
 
 import Render.DisplayManager;
+import Render.MasterRender;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Main {
 
     public static void main(String[] args){
 
         DisplayManager.createDisplay();
+
+        MasterRender renderer = new MasterRender();
+
+        long window = DisplayManager.getWindow();
+
+        while (!glfwWindowShouldClose(window)) {
+
+            renderer.prepare();
+            DisplayManager.updateDisplay();
+
+        }
 
     }
 
