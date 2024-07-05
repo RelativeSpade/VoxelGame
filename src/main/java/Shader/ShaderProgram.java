d0e7b4a11314cba0bcd48b2180b0b9b91b44da7d
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 09:09:31 -0400

    change: Shader constructor finished

--- src/main/java/Shader/ShaderProgram.java
+++ src/main/java/Shader/ShaderProgram.java
@@ -10,6 +10,23 @@ import java.io.InputStreamReader;
 
 public abstract class ShaderProgram {
 
+    int programID;
+    int vertexShaderID;
+    int fragmentShaderID;
+
+    public ShaderProgram(String vertexFile, String fragmentFile) {
+
+        programID = GL20.glCreateProgram();
+        vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
+        fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
+
+        GL20.glAttachShader(programID, vertexShaderID);
+        GL20.glAttachShader(programID, fragmentShaderID);
+        GL20.glLinkProgram(programID);
+        GL20.glValidateProgram(programID);
+
+    }
+
     private int loadShader(String file, int type) {
 
         StringBuilder shaderSource = new StringBuilder();
