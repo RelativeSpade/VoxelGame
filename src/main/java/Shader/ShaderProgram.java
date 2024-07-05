dd5559ac1159aa0637da2a20ca81a78f843e287b
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 09:03:57 -0400

    change: finished shader class

--- src/main/java/Shader/ShaderProgram.java
+++ src/main/java/Shader/ShaderProgram.java
@@ -1,4 +1,45 @@
 package Shader;
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public abstract class ShaderProgram {
+
+    private int loadShader(String file, int type) {
+
+        StringBuilder shaderSource = new StringBuilder();
+        InputStream in = Class.class.getResourceAsStream(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                shaderSource.append(line).append("//\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Could not load shader file!");
+            System.exit(-1);
+        }
+
+        int shaderID = GL20.glCreateShader(type);
+        GL20.glShaderSource(shaderID, shaderSource);
+        GL20.glCompileShader(shaderID);
+
+        if (GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
+
+            System.out.println(GL20.glGetShaderInfoLog(shaderID, 1000));
+            System.err.println("Could not compile shader!");
+            System.exit(-1);
+
+        }
+
+        return shaderID;
+    }
 }
