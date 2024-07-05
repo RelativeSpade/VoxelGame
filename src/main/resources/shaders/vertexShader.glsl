8824e319333a123d547db4c0698ddb640abcab6e
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 10:35:37 -0400

    change: made shader slightly brighter

--- src/main/resources/shaders/vertexShader.glsl
+++ src/main/resources/shaders/vertexShader.glsl
@@ -7,6 +7,6 @@ out vec3 color;
 void main(void) {
 
     gl_Position = vec4(position, 1.0);
-    color = vec3(position.x, position.y, position.z);
+    color = vec3(position.x + 0.5, position.y + 0.5, position.z + 0.5);
 
 }
