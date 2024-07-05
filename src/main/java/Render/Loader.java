c069c211064f5f63dfd3f65052544c5f83a4103e
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 05:53:53 -0400

    add: loader class

---
+++ src/main/java/Render/Loader.java
@@ -0,0 +1,62 @@
+package Render;
+
+import Models.RawModel;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loader {
+
+    static List<Integer> vaos = new ArrayList<Integer>();
+    static List<Integer> vbos = new ArrayList<Integer>();
+
+    public RawModel loadToVAO(float[] verticies) {
+        int vaoID = createVAO();
+        storeDataInAttributeList(verticies, 0, 3);
+        GL30.glBindVertexArray(0);
+
+        return new RawModel(vaoID, verticies.length);
+    }
+
+    private int createVAO() {
+        int vaoID = GL30.glGenVertexArrays();
+        GL30.glBindVertexArray(vaoID);
+
+        return vaoID;
+    }
+
+    private void storeDataInAttributeList(float[] data, int attributeNumber, int dimensions) {
+        int vboID = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+        FloatBuffer buffer = storeDataInFloatBuffer(data);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(attributeNumber, dimensions, GL11.GL_FLOAT, false, 0, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+    }
+
+    private FloatBuffer storeDataInFloatBuffer(float[] data) {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+
+        return  buffer;
+    }
+
+    public void cleanUp() {
+
+        for(int vao : vaos){
+            GL30.glDeleteVertexArrays(vao);
+        }
+
+        for(int vbo : vbos){
+            GL15.glDeleteBuffers(vbo);
+        }
+
+    }
+}
