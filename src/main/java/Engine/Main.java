9469258589ef2d266ac4dd29cb48dac5ce736417
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-05 04:47:12 -0400

    main

---
+++ src/main/java/Engine/Main.java
@@ -0,0 +1,13 @@
+package Engine;
+
+import org.spade.Render.DisplayManager;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        DisplayManager.createDisplay();
+
+    }
+
+}
