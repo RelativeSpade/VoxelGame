d5d6fceae9c6c33abb4545893edc76f5ad2303e1
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-07 04:49:42 -0400

    add: transformation stuff.

---
+++ src/main/java/Toolbox/Math/Transformation.java
@@ -0,0 +1,19 @@
+package Toolbox.Math;
+
+public class Transformation {
+
+    public static Matrix4D createTransformationMatrix(Vec3D translation, float rX, float rY, float rZ, float scale) {
+
+        Matrix4D matrix = new Matrix4D();
+        matrix.setIdentity();
+
+        Matrix4D.translate(translation, matrix, matrix);
+        Matrix4D.rotate(rX, new Vec3D(1, 0, 0), matrix, matrix);
+        Matrix4D.rotate(rY, new Vec3D(0, 1, 0), matrix, matrix);
+        Matrix4D.rotate(rZ, new Vec3D(0, 0, 1), matrix, matrix);
+        Matrix4D.scale(new Vec3D(scale, scale, scale), matrix, matrix);
+
+        return matrix;
+
+    }
+}
