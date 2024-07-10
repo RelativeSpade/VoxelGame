f8fddb159adcf4cb90ebea668148f85aff3ec85a
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-10 01:33:32 -0400

    change: code optimization

--- src/main/java/Render/MasterRender.java
+++ src/main/java/Render/MasterRender.java
@@ -26,6 +26,7 @@ public class MasterRender {
     public void prepare() {
 
         GL11.glEnable(GL11.GL_DEPTH_TEST);
+
         GL11.glClearColor(0.4f, 0.7f, 1.0f, 1);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
