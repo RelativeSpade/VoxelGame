3dd2bb20ebafba47891fa45edd995257c0f49313
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-15 04:53:54 -0400

    change: implemented perlin noise and cleaned main up some more.

--- src/main/java/Engine/Main.java
+++ src/main/java/Engine/Main.java
@@ -13,6 +13,7 @@ import Shaders.StaticShader;
 import Textures.ModelTexture;
 import Toolbox.Math.Vec3;
 import Toolbox.Necessities.Mouse;
+import Toolbox.Necessities.PerlinNoiseGenerator;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,12 +28,13 @@ public class Main {
     public static int FPS = -1;
     public static double previousTime = glfwGetTime();
     public static float VOXEL_SIZE = 1f;
+    public static int CHUNK_SIZE = 32;
     static List<ChunkMesh> chunks = Collections.synchronizedList(new ArrayList<>());
     static List<Entity> entities = new ArrayList<>();
     static Vec3 camPos = new Vec3(0, 0, 0);
     static List<Vec3> usedPos = Collections.synchronizedList(new ArrayList<>());
 
-    static final int WORLD_SIZE = 4 * 16;
+    static final int WORLD_SIZE = 4 * CHUNK_SIZE;
     public static void main(String[] args){
 
         DisplayManager.createDisplay();
@@ -51,27 +53,29 @@ public class Main {
 
         Camera camera = new Camera(new Vec3(0, 0, 0), 0, 0, 0);
 
+        PerlinNoiseGenerator perlin = new PerlinNoiseGenerator();
+
         new Thread(() -> {
 
             while(!glfwWindowShouldClose(window)) {
 
-                for (int x = (int) ((camPos.x - WORLD_SIZE) / 16); x < ((camPos.x + WORLD_SIZE) / 16); x++){
-                    for (int z = (int) ((camPos.z - WORLD_SIZE) / 16); z < ((camPos.z + WORLD_SIZE) / 16); z++) {
+                for (int x = (int) ((camPos.x - WORLD_SIZE) / CHUNK_SIZE); x < ((camPos.x + WORLD_SIZE) / CHUNK_SIZE); x++){
+                    for (int z = (int) ((camPos.z - WORLD_SIZE) / CHUNK_SIZE); z < ((camPos.z + WORLD_SIZE) / CHUNK_SIZE); z++) {
 
-                        if(!usedPos.contains(new Vec3(x*16, 0,z*16))) {
+                        if(!usedPos.contains(new Vec3(x*CHUNK_SIZE, 0,z*CHUNK_SIZE))) {
 
                             List<Block> blocks = new ArrayList<>();
 
-                            for(int i =0; i < 16; i++){
-                                for(int j = 0; j < 16; j++){
+                            for(int i = 0; i < CHUNK_SIZE; i++){
+                                for(int j = 0; j < CHUNK_SIZE; j++){
 
-                                    blocks.add(new Block(i,0,j, Block.TYPE.DIRT));
+                                    blocks.add(new Block(i, (int) perlin.generateHeight(i + (x * CHUNK_SIZE), j + (z * CHUNK_SIZE)),j, Block.TYPE.DIRT));
 
                                 }
                             }
-                            Chunk tempChunk = new Chunk(blocks, new Vec3(x*16, 0, z*16));
+                            Chunk tempChunk = new Chunk(blocks, new Vec3(x*CHUNK_SIZE, 0, z*CHUNK_SIZE));
                             chunks.add(new ChunkMesh(tempChunk));
-                            usedPos.add(new Vec3(x*16, 0,z*16));
+                            usedPos.add(new Vec3(x*CHUNK_SIZE, 0,z*CHUNK_SIZE));
 
                         }
                     }
@@ -100,7 +104,6 @@ public class Main {
             if(index < chunks.size()){
 
                 ChunkMesh chunkMesh = chunks.get(index);
-
                 RawModel tempModel = loader.loadToVAO(chunkMesh.positions, chunkMesh.uvs);
                 TextureModel textureModel1 = new TextureModel(tempModel, texture);
                 Entity entity = new Entity(textureModel1, chunkMesh.chunk.getOrigin(), 0, 0, 0, VOXEL_SIZE);
